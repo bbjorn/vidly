64fcb4275d492d29e5279f58e852cd0a7e4b7088
bbjorn <bbjorn@student.chalmers.se>
2020-06-15 16:18:37 +0200

    Added icons to tableheader

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSortDown, faSortUp);
 
 // Colums: array
 // SortColumn: object
@@ -15,16 +20,27 @@ class TableHeader extends Component {
     }
     this.props.onSort(sortColumn);
   };
+
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc")
+      return <FontAwesomeIcon icon={faSortUp} aria-hidden="true" />;
+    return <FontAwesomeIcon icon={faSortDown} aria-hidden="true" />;
+  };
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map((column) => (
             <th
+              className="clickable"
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
               {column.label}
+              {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
