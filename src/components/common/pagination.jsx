b33eb78907c522d2f193460bbacef4bfa47c3f0c
bbjorn <bbjorn@student.chalmers.se>
2020-06-15 08:53:08 +0200

    Made pagination into a sfc

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,30 +1,27 @@
 import React, { Component } from "react";
 
-class Pagination extends Component {
-  state = {};
-  render() {
-    return (
-      <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className="page-item">
-            <a className="page-link" href="#">
-              1
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              3
-            </a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const Pagination = () => {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            3
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Pagination;
