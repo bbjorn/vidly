ebab4a77e5ec737c563ee8d75b59c2d06d20f1d8
bbjorn <bbjorn@student.chalmers.se>
2020-06-15 16:30:47 +0200

    Update table.jsx

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import TableHeader from "./tableHeader.jsx";
+import TableBody from "./tableBody";
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
       <TableBody columns={columns} data={data} />
