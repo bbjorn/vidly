66878d386146f8db1373c0d5d17ff8040e070f8c
bbjorn <bbjorn@student.chalmers.se>
2020-06-18 18:02:33 +0200

    Did Exercise on routing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import Movies from "./components/movies";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import Navbar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 library.add(faHeart, faHeartRegular);
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/notfound" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/NotFound" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 //import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
