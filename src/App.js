bf3f896c77a5d9e8e00358fdea9a445fab5a5b91
bbjorn <bbjorn@student.chalmers.se>
2020-06-22 09:02:59 +0200

    Built login form with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./App.css";
-import Movies from "./components/movies";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+
+import Movies from "./components/movies";
 import Navbar from "./components/navbar";
-import { Route, Switch, Redirect } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+
+import "./App.css";
 
 library.add(faHeart, faHeartRegular);
 function App() {
@@ -18,6 +21,7 @@ function App() {
       <Navbar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
