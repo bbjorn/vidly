ad8514c328cf612a44a52cd1238ed3bba79775b7
bbjorn <bbjorn@student.chalmers.se>
2020-07-10 12:26:25 +0200

    Did an exercise to connect Vidly to a database

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
-
+import { ToastContainer } from "react-toastify";
 import Movies from "./components/movies";
 import Navbar from "./components/navbar";
 import Customers from "./components/customers";
@@ -13,12 +13,14 @@ import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 library.add(faHeart, faHeartRegular);
 function App() {
   return (
     <React.Fragment>
+      <ToastContainer />
       <Navbar />
       <main className="container">
         <Switch>
