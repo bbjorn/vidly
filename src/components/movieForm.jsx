8161269fe2cfd6a947f0c9d5b613db04e3245f57
bbjorn <bbjorn@student.chalmers.se>
2020-07-14 09:50:09 +0200

    Did exercise on connecting movie form to backend and refactored code

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
-import { getGenres } from "./../services/fakeGenreService";
-import { getMovie, saveMovie } from "./../services/fakeMovieService";
+import { getGenres } from "./../services/genreService";
+import { getMovie, saveMovie } from "./../services/movieService";
 
 class MovieForm extends Form {
   state = {
@@ -24,17 +24,27 @@ class MovieForm extends Form {
     dailyRentalRate: Joi.number().min(1).max(10).required().label("Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenres() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
+  }
 
-    this.setState({ data: this.mapToViewModel(movie) });
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
@@ -47,7 +57,7 @@ class MovieForm extends Form {
     };
   }
 
-  doSubmit = () => {
+  doSubmit = async () => {
     saveMovie(this.state.data);
 
     this.props.history.push("/movies");
--- src/services/logService.js
+++ src/services/logService.js
@@ -1,21 +1,10 @@
-//const Raven = require("raven-js");
-
-//import Raven from "raven-js";
 
 function init() {
-  /* Raven.config(
-    "https://0941cab473bf4f07a83251bfe05a054b@o417122.ingest.sentry.io/5315383",
-    {
-      release: "1-0-0",
-      environment: "development-test",
-    }
-  ).install(); */
+
 }
 
 function log(error) {
   console.error(error);
-
-  //Raven.captureException(error);
 }
 
 export default {
