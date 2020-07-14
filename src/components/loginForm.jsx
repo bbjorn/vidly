54524cb0b72a51b21e2fa5a98e3a5da81b6b7905
bbjorn <bbjorn@student.chalmers.se>
2020-07-14 10:31:46 +0200

    Did exercise on submitting the login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
+import { login } from "./../services/authService";
 
 class LoginForm extends Form {
   username = React.createRef();
@@ -12,9 +13,9 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  doSubmit = () => {
-    // Call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    const { data } = this.state;
+    await login(data.username, data.password);
   };
 
   render() {
